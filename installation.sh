f6354d9c29d5d7fccc66705d8b1a7cf90b5e319a
nijat1986 <nijat.jafarovdevops@gmail.com>
2023-03-20 21:06:47 +0000

    added tfenv

---
+++ installation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+# bash installation.sh
+# source ~/.bashrc
