b85d002914242c15462c9c9c486fd9d1baffc824
nijat1986 <nijat.jafarovdevops@gmail.com>
2023-03-20 22:01:22 +0000

    added aws provider

--- Class1/provider.tf
+++ Class1/provider.tf
@@ -2,3 +2,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+# # Configure the Microsoft Azure Provider
+# provider "azurerm" {
+#   features {}
+# }
+
+# provider "google" {
+#   project     = "my-project-id"
+#   region      = "us-central1"
+# }
